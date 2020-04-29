21bdcc893753f41e768837ac6fc520cd7bc390e7
priyanshbendre <priyanshbendre@gmail.com>
2020-04-28 18:03:44 -0700

    initial commit

---
+++ selenium_test.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+
+options = webdriver.ChromeOptions()
+options.add = "/Users/priyanshbendre/Priyansh/selenium_drivers/chromedriver"
+driver = webdriver.Chrome(chrome_options=options, executable_path="/Users/priyanshbendre/Priyansh/selenium_drivers/chromedriver")
+
+driver.get("https://google.com")
